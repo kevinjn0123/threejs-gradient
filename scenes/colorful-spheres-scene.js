49a8ec9cbf045ff258856323890945a53f5343fc
ruucm <ruucm.a@gmail.com>
2021-05-23 16:27:25 +0900

    refactor: seperate colorful-spheres-scene

---
+++ scenes/colorful-spheres-scene.js
@@ -0,0 +1,75 @@
+import * as THREE from "https://cdn.skypack.dev/pin/three@v0.128.0-SK0zhlI7UZNd0gIQdpJa/mode=imports/optimized/three.js"
+import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls"
+import { vertexShader, fragmentShader } from "../shaders/spheres.js"
+
+const settings = {
+  speed: 0.2,
+  density: 1.5,
+  strength: 0.2,
+}
+
+export class Scene {
+  constructor() {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    this.camera.position.set(0, 0, 4)
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 1.5))
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setClearColor("green", 1)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+    this.clock = new THREE.Clock()
+
+    this.init()
+  }
+
+  init() {
+    this.addCanvas()
+    this.addMeshElements()
+    this.animationLoop()
+  }
+
+  addCanvas() {
+    const canvas = this.renderer.domElement
+    document.body.appendChild(canvas)
+  }
+
+  addMeshElements() {
+    const geometry = new THREE.IcosahedronBufferGeometry(1, 64)
+    const material = new THREE.ShaderMaterial({
+      vertexShader: vertexShader(),
+      fragmentShader: fragmentShader(),
+      uniforms: {
+        uTime: { value: 0 },
+        uSpeed: { value: settings.speed },
+        uNoiseDensity: { value: settings.density },
+        uNoiseStrength: { value: settings.strength },
+      },
+      wireframe: true,
+    })
+    this.mesh = new THREE.Mesh(geometry, material)
+    this.scene.add(this.mesh)
+  }
+
+  animationLoop() {
+    // ------------------------- ANIMATE SPHERES -------------------------------
+    this.mesh.material.uniforms.uTime.value = this.clock.getElapsedTime()
+    this.mesh.material.uniforms.uSpeed.value = settings.speed
+    this.mesh.material.uniforms.uNoiseDensity.value = settings.density
+    this.mesh.material.uniforms.uNoiseStrength.value = settings.strength
+
+    // ------------------------- START ANIMATE -------------------------------
+    window.requestAnimationFrame(this.animationLoop.bind(this)) // bind "this" to keep pointing the constructed scene
+    this.renderer.render(this.scene, this.camera)
+
+    this.controls.update()
+  }
+}
