a5b4a155e9adbd1130790e454b0bcf92b418383c
ruucm <ruucm.a@gmail.com>
2021-05-28 14:22:52 +0900

    add gui to sphereElement

--- meshes/sphere-element.js
+++ meshes/sphere-element.js
@@ -1,12 +1,16 @@
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.128.0-SK0zhlI7UZNd0gIQdpJa/mode=imports/optimized/three.js"
 import { vertexShader, fragmentShader } from "../shaders/spheres.js"
+import * as dat from "https://cdn.skypack.dev/dat.gui@0.7.7"
 
-// TODO: add dat GUI or https://github.com/pmndrs/leva
 const settings = {
   speed: 0.2,
   density: 1.5,
   strength: 0.2,
 }
+const gui = new dat.GUI()
+gui.add(settings, "speed", 0.1, 1, 0.01)
+gui.add(settings, "density", 0, 10, 0.01)
+gui.add(settings, "strength", 0, 2, 0.01)
 
 export const sphereElement = function () {
   const geometry = new THREE.IcosahedronBufferGeometry(1, 64)
