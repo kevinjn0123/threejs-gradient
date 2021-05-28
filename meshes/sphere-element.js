4c736341a6519b1dcbf839eb0f4c3f914616666b
ruucm <ruucm.a@gmail.com>
2021-05-28 16:19:32 +0900

    seperate fragmentShader as an external file(.frag)

--- meshes/sphere-element.js
+++ meshes/sphere-element.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.128.0-SK0zhlI7UZNd0gIQdpJa/mode=imports/optimized/three.js"
-import { vertexShader, fragmentShader } from "../shaders/spheres.js"
+import { vertexShader, fragmentShader } from "../shaders/spheres/index.js"
 import * as dat from "https://cdn.skypack.dev/dat.gui@0.7.7"
 
 const settings = {
@@ -26,7 +26,7 @@ export const sphereElement = function () {
   const geometry = new THREE.IcosahedronBufferGeometry(1, 64)
   const material = new THREE.ShaderMaterial({
     vertexShader: vertexShader(),
-    fragmentShader: fragmentShader(),
+    fragmentShader,
     uniforms: {
       uTime: { value: 0 },
       uSpeed: { value: settings.speed },
--- shaders/spheres/index.js
+++ shaders/spheres/index.js
@@ -1,3 +1,5 @@
+import fetch from "../../web_modules/whatwg-fetch.js"
+
 const noise = /* js */ `
   // GLSL textureless classic 3D noise "cnoise",
   // with an RSL-style periodic variant "pnoise".
@@ -159,34 +161,6 @@ export function vertexShader() {
 `
 }
 
-export function fragmentShader() {
-  return /* js */ `
-  varying float vDistort;
-  
-  uniform float uTime;
-  uniform float uIntensity;
-
-  vec3 cosPalette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
-    return a + b * cos(6.28318 * (c * t + d));
-  }  
-  
-  void main() {
-    float distort = vDistort * uIntensity;
-    // These values are my fav combination, 
-    // they remind me of Zach Lieberman's work.
-    // You can find more combos in the examples from IQ:
-    // https://iquilezles.org/www/articles/palettes/palettes.htm
-    // Experiment with these!
-    vec3 brightness = vec3(0.5, 0.5, 0.5);
-    vec3 contrast = vec3(0.5, 0.5, 0.5);
-    vec3 oscilation = vec3(1.0, 1.0, 1.0);
-    vec3 phase = vec3(0.0, 0.1, 0.2);
-      
-      
-    // Pass the distortion as input of cospalette
-    vec3 color = cosPalette(distort, brightness, contrast, oscilation, phase);
-
-    gl_FragColor = vec4(color, 1.0);
-  }   
-`
-}
+export const fragmentShader = await fetch(
+  `/shaders/spheres/fragmentShader.frag`
+).then((res) => res.text())
