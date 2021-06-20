b082ac561a1f042e650cb76420ff7c8d7560e430
ruucm <ruucm.a@gmail.com>
2021-06-20 15:16:53 +0900

    add text element

--- scenes/simple-scene-2.js
+++ scenes/simple-scene-2.js
@@ -5,7 +5,9 @@ import {
   planeElement,
   coneElement,
   cylinderElement,
+  textElement,
 } from "../meshes/simple-element-2.js"
+import { vertexShader, fragmentShader } from "../shaders/turbulence/index.js"
 
 export class Scene {
   constructor() {
@@ -53,19 +55,88 @@ export class Scene {
   }
 
   addMeshElements() {
-    const plane = new cylinderElement()
-    this.mesh = plane.mesh
-    console.log("this.mesh", this.mesh)
-    this.meshSettings = plane.settings
+    const settings = {
+      speed: 0.2,
+      density: 1.5,
+      // strength: 1.8,
+      // frequency: 3.0,
+      // intensity: 7.0,
+    }
 
-    this.scene.add(this.mesh)
+    // const plane = new textElement()
+    // this.mesh = plane.mesh
+    // console.log("this.mesh", this.mesh)
+    // this.meshSettings = plane.settings
+
+    const loader = new THREE.FontLoader()
+
+    loader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+      const material = new THREE.ShaderMaterial({
+        side: THREE.DoubleSide,
+        vertexShader,
+        // fragmentShader,
+        uniforms: {
+          uTime: { value: 0 },
+          uSpeed: { value: settings.speed },
+          // uNoiseDensity: { value: settings.density },
+          // uNoiseStrength: { value: settings.strength },
+          // uFrequency: { value: settings.frequency },
+          // uIntensity: { value: settings.intensity },
+          // resolution: { value: new THREE.Vector3() },
+        },
+        wireframe: true,
+      })
+
+      // const geometry = new THREE.TextGeometry("Hello three.js!", {
+      //   font: font,
+      //   size: 80,
+      //   height: 5,
+      //   curveSegments: 12,
+      //   bevelEnabled: true,
+      //   bevelThickness: 10,
+      //   bevelSize: 8,
+      //   bevelOffset: 0,
+      //   bevelSegments: 5,
+      // })
+
+      // // const geometry = new THREE.CylinderGeometry(5, 5, 20, 32)
+      // this.mesh = new THREE.Mesh(geometry, material)
+
+      //
+
+      const textGeo = new THREE.TextGeometry("Hello three.js!", {
+        font: font,
+
+        size: 80,
+        height: 5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 8,
+        bevelOffset: 0,
+        bevelSegments: 5,
+      })
+
+      textGeo.computeBoundingBox()
+
+      const centerOffset =
+        -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x)
+
+      this.mesh = new THREE.Mesh(textGeo, material)
+      console.log("this.mesh", this.mesh)
+      this.scene.add(this.mesh)
+    })
+
     // this.mesh.rotation.x = Math.PI / 2
   }
 
   animationLoop() {
     // ------------------------- ANIMATE PLANE -------------------------------
-    this.mesh.material.uniforms.uTime.value = this.clock.getElapsedTime()
-    this.mesh.material.uniforms.uSpeed.value = this.meshSettings.speed
+    if (this.mesh) {
+      this.mesh.material.uniforms.uTime.value = this.clock.getElapsedTime()
+      // this.mesh.material.uniforms.uSpeed.value = this.meshSettings.speed
+    }
+
     // this.mesh.material.uniforms.uNoiseDensity.value = this.meshSettings.density
     // this.mesh.material.uniforms.uNoiseStrength.value =
     //   this.meshSettings.strength
