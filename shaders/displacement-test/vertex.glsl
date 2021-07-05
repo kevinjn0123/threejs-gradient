085e211a18c052e7fbdaac97116084cafa297178
ruucm <ruucm.a@gmail.com>
2021-07-05 17:09:51 +0900

    add gradient fragment shader

--- shaders/displacement-test/vertex.glsl
+++ shaders/displacement-test/vertex.glsl
@@ -9,7 +9,6 @@ uniform float uNoiseStrength;
 
 void main() {
   vUv = uv;
-  fNormal = normal;
   // TODO: 그린 캔버스의 이미지를, texture2D 로 활용하기
   vec4 noiseTex = texture2D(texture1, vUv);
   texNoise = noiseTex.r;
@@ -19,10 +18,15 @@ void main() {
   // float displacement = pnoise((normal + uTime), vec3(10.0));
 
   // float normalDisplacement = cnoise(position + uTime);
-  float normalDisplacement =
-      pnoise(position + uTime, vec3(1.0)) * uNoiseStrength;
 
+  float normalDisplacement =
+      pnoise(position + uTime, vec3(0.0)) * uNoiseStrength;
   vec3 newPosition = position + normal * scale * texNoise * normalDisplacement;
+
+  // float displacement = 0.75 * cnoise(0.43 * position + uTime);
+  // vec3 newPosition = position + normal * displacement * uNoiseStrength;
+
+  fNormal = normal * normalDisplacement;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 }
 
