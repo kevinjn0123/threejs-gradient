81e87a33ded3e98ecb7822abfa603ff9b258da0b
ruucm <ruucm.a@gmail.com>
2021-07-11 15:41:05 +0900

    change colors

--- meshes/consts.js
+++ meshes/consts.js
@@ -4,21 +4,21 @@ export const getVertices = (points) => [
   {
     x: points.point_1_x,
     y: points.point_1_y,
-    color: new THREE.Color("hsl(20, 100%, 71%)"),
+    color: new THREE.Color("hsl(20, 100%, 50%)"),
   },
   {
     x: points.point_2_x,
     y: points.point_2_y,
-    color: new THREE.Color("hsl(40, 100%, 71%)"),
+    color: new THREE.Color("hsl(40, 100%, 50%)"),
   },
   {
     x: points.point_3_x,
     y: points.point_3_y,
-    color: new THREE.Color("hsl(200, 100%, 71%)"),
+    color: new THREE.Color("hsl(100, 100%, 50%)"),
   },
   {
     x: points.point_4_x,
     y: points.point_4_y,
-    color: new THREE.Color("hsl(140, 100%, 65%)"),
+    color: new THREE.Color("hsl(70, 100%, 70%)"),
   },
 ]
