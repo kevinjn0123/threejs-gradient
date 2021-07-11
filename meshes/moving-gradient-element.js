e886d34bb2ad660090e021a72f98241c1f51897c
ruucm <ruucm.a@gmail.com>
2021-07-11 14:36:23 +0900

    rm unusings

--- meshes/moving-gradient-element.js
+++ meshes/moving-gradient-element.js
@@ -8,14 +8,12 @@ import {
 import { CustomMat } from "./utils.js"
 
 const settings = {
-  speed: 0.2,
   strength: 2.0,
 }
 
 const gui = new dat.GUI()
 
 const folder1 = gui.addFolder("Noise")
-folder1.add(settings, "speed", 0.1, 1, 0.01)
 folder1.add(settings, "strength", 0, 20, 0.01)
 
 export const sphereElement = function () {
@@ -27,7 +25,6 @@ export const sphereElement = function () {
     uniforms: {
       uTime: { value: 0 },
       uNoiseStrength: { value: settings.strength },
-      // uSpeed: { value: settings.speed },
       texture1: { type: "t", value: null },
       scale: { type: "f", value: 1.0 },
 
@@ -59,108 +56,4 @@ export const planeElement = function () {
 
   const geometry = new THREE.PlaneGeometry(20, 20, 100, 100)
   this.mesh = new THREE.Mesh(geometry, material)
-}
-
-export const boxElement = function () {
-  const material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
-    vertexShader,
-    // fragmentShader,
-    uniforms: {
-      uTime: { value: 0 },
-      uSpeed: { value: settings.speed },
-      // uNoiseDensity: { value: settings.density },
-      // uNoiseStrength: { value: settings.strength },
-      // uFrequency: { value: settings.frequency },
-      // uIntensity: { value: settings.intensity },
-      // resolution: { value: new THREE.Vector3() },
-    },
-    wireframe: true,
-  })
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  this.mesh = new THREE.Mesh(geometry, material)
-
-  this.settings = settings
-}
-
-export const coneElement = function () {
-  const material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
-    vertexShader,
-    // fragmentShader,
-    uniforms: {
-      uTime: { value: 0 },
-      uSpeed: { value: settings.speed },
-      // uNoiseDensity: { value: settings.density },
-      // uNoiseStrength: { value: settings.strength },
-      // uFrequency: { value: settings.frequency },
-      // uIntensity: { value: settings.intensity },
-      // resolution: { value: new THREE.Vector3() },
-    },
-    wireframe: true,
-  })
-  const geometry = new THREE.ConeGeometry(5, 20, 32)
-  this.mesh = new THREE.Mesh(geometry, material)
-
-  this.settings = settings
-}
-
-export const cylinderElement = function () {
-  const material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
-    vertexShader,
-    // fragmentShader,
-    uniforms: {
-      uTime: { value: 0 },
-      uSpeed: { value: settings.speed },
-      // uNoiseDensity: { value: settings.density },
-      // uNoiseStrength: { value: settings.strength },
-      // uFrequency: { value: settings.frequency },
-      // uIntensity: { value: settings.intensity },
-      // resolution: { value: new THREE.Vector3() },
-    },
-    wireframe: true,
-  })
-  // const geometry = new THREE.ConeGeometry(5, 20, 32)
-  const geometry = new THREE.CylinderGeometry(5, 5, 20, 32)
-  this.mesh = new THREE.Mesh(geometry, material)
-
-  this.settings = settings
-}
-
-export const textElement = function (font) {
-  this.settings = settings
-
-  const material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
-    vertexShader,
-    // fragmentShader,
-    uniforms: {
-      uTime: { value: 0 },
-      uSpeed: { value: settings.speed },
-      // uNoiseDensity: { value: settings.density },
-      // uNoiseStrength: { value: settings.strength },
-      // uFrequency: { value: settings.frequency },
-      // uIntensity: { value: settings.intensity },
-      // resolution: { value: new THREE.Vector3() },
-    },
-    wireframe: true,
-  })
-
-  const textGeo = new THREE.TextGeometry("Hello three.js!", {
-    font: font,
-    size: 80,
-    height: 5,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 10,
-    bevelSize: 8,
-    bevelOffset: 0,
-    bevelSegments: 5,
-  })
-
-  textGeo.computeBoundingBox()
-
-  this.mesh = new THREE.Mesh(textGeo, material)
 }
