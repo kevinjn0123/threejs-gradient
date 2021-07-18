5b28aa722bf213477a2b580daf9abe022f94122e
ruucm <ruucm.a@gmail.com>
2021-07-18 15:46:42 +0900

    up

--- react/pages/scene-1.tsx
+++ react/pages/scene-1.tsx
@@ -2,19 +2,58 @@ import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import { PlaneElement } from "../components/moving-gradient-elements"
 import { useShader } from "../hooks/use-shader"
+import * as THREE from "three"
+
+const getVertices = (points) => [
+  {
+    x: points.point_1_x,
+    y: points.point_1_y,
+    color: new THREE.Color("hsl(20, 100%, 50%)"),
+  },
+  {
+    x: points.point_2_x,
+    y: points.point_2_y,
+    color: new THREE.Color("hsl(0, 100%, 50%)"),
+  },
+  {
+    x: points.point_3_x,
+    y: points.point_3_y,
+    color: new THREE.Color("hsl(130, 100%, 50%)"),
+  },
+  {
+    x: points.point_4_x,
+    y: points.point_4_y,
+    color: new THREE.Color("hsl(230, 100%, 50%)"),
+  },
+]
 
 export default function Scene1() {
   const fragmentShader = useShader({
-    publicPath: "/shaders/turbulence/fragment.frag",
+    publicPath: "/shaders/moving-gradient/fragment.frag",
   })
-  const noises = useShader({ publicPath: "/shaders/turbulence/noises.glsl" })
-  const vertex = useShader({ publicPath: "/shaders/turbulence/vertex.glsl" })
+  const noises = useShader({
+    publicPath: "/shaders/moving-gradient/noises.glsl",
+  })
+  const vertex = useShader({
+    publicPath: "/shaders/moving-gradient/vertex.glsl",
+  })
   const vertexShader = `
     ${noises}
     ${vertex}
     `
 
   console.log("fragmentShader", fragmentShader)
+
+  const points = {
+    point_1_x: 0.2,
+    point_1_y: 0.2,
+    point_2_x: 0.8,
+    point_2_y: 0.2,
+    point_3_x: 0.2,
+    point_3_y: 0.8,
+    point_4_x: 0.8,
+    point_4_y: 0.8,
+  }
 
   return (
     <div
@@ -25,16 +64,26 @@ export default function Scene1() {
       }}
     >
       <Canvas>
+        {/* addMeshElements */}
         <mesh>
           <PlaneElement />
           <shaderMaterial
-            side="DoubleSide"
+            side={THREE.DoubleSide}
             fragmentShader={fragmentShader}
             vertexShader={vertexShader}
+            uniforms={{
+              uTime: { value: 0 },
+              uNoiseStrength: { value: 5 },
+              texture1: { type: "t", value: null },
+              scale: { type: "f", value: 1.0 },
+              vertices: { value: getVertices(points) },
+              resolution: { value: new THREE.Vector2(400, 400) },
+            }}
           />
         </mesh>
-        <ambientLight args={[0xff0000]} intensity={0.1} />
-        <directionalLight color="blue" position={[0, 0, 5]} intensity={0.5} />
+        {/* addLights */}
+        <hemisphereLight args={[0xffffff, 0x000000, 1.4]} />
+        <pointLight args={[0xffffff, 0.5]} />
         {/* @ts-ignore */}
         <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
       </Canvas>
