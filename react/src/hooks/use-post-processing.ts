382595555079bb61339557444eab404dd10f7698
ruucm <ruucm.a@gmail.com>
2021-08-08 17:34:13 +0900

    fmt

--- react/src/hooks/use-post-processing.ts
+++ react/src/hooks/use-post-processing.ts
@@ -1,18 +1,6 @@
-import { GUIGradient } from '@/components/dom/gui-gradient'
-import dynamic from 'next/dynamic'
-import { Environment } from '@react-three/drei'
-import { Suspense, useEffect } from 'react'
-import * as THREE from 'three'
-import { useFrame, useThree } from '@react-three/fiber'
-import * as PP from '@/components/pp'
 import { HalftonePass } from '@/components/pp/lib/HalftonePassO'
-// import { BlendFunction } from 'postprocessing'
-// import {
-//   EffectComposer,
-//   RenderPass,
-//   EffectPass,
-//   NormalPass,
-// } from 'postprocessing'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect } from 'react'
 import { EffectComposer } from '../components/pp/lib/postprocessing/EffectComposer'
 import { RenderPass } from '../components/pp/lib/postprocessing/RenderPass'
 
--- react/src/pages/gradient-scene.tsx
+++ react/src/pages/gradient-scene.tsx
@@ -1,21 +1,9 @@
 import { GUIGradient } from '@/components/dom/gui-gradient'
-import dynamic from 'next/dynamic'
-import { Environment } from '@react-three/drei'
-import { Suspense, useEffect } from 'react'
-import * as THREE from 'three'
-import { useFrame, useThree } from '@react-three/fiber'
-import * as PP from '@/components/pp'
-import { HalftonePass } from '@/components/pp/lib/HalftonePassO'
-// import { BlendFunction } from 'postprocessing'
-// import {
-//   EffectComposer,
-//   RenderPass,
-//   EffectPass,
-//   NormalPass,
-// } from 'postprocessing'
-import { EffectComposer } from '../components/pp/lib/postprocessing/EffectComposer'
-import { RenderPass } from '../components/pp/lib/postprocessing/RenderPass'
 import { usePostProcessing } from '@/hooks/use-post-processing'
+import { Environment } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
 
 const GradientMesh = dynamic(() => import('@/components/canvas/GradientMesh'), {
   ssr: false,
