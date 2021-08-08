4efb4c856db2610f350910359e90d81b6c679ec0
ruucm <ruucm.a@gmail.com>
2021-08-08 17:42:35 +0900

    wrap with useMemo

--- react/src/hooks/use-post-processing.ts
+++ react/src/hooks/use-post-processing.ts
@@ -1,28 +1,40 @@
 import { HalftonePass } from '@/components/pp/lib/HalftonePassO'
 import { useFrame, useThree } from '@react-three/fiber'
-import { useEffect } from 'react'
-import { EffectComposer } from '../components/pp/lib/postprocessing/EffectComposer'
+import { useEffect, useMemo } from 'react'
+import { EffectComposer as EffectComposerImpl } from '../components/pp/lib/postprocessing/EffectComposer'
 import { RenderPass } from '../components/pp/lib/postprocessing/RenderPass'
 
 export function usePostProcessing() {
   const { gl, scene, camera, size } = useThree()
 
-  const composer = new EffectComposer(gl)
-  composer.addPass(new RenderPass(scene, camera))
-  const halftoneParams = {
-    shape: 1,
-    radius: 2,
-    rotateR: Math.PI / 12,
-    rotateB: (Math.PI / 12) * 2,
-    rotateG: (Math.PI / 12) * 3,
-    scatter: 1,
-    blending: 1,
-    blendingMode: 1,
-    greyscale: false,
-    disable: false,
-  }
-  const halftonePass = new HalftonePass(size.width, size.height, halftoneParams)
-  composer.addPass(halftonePass)
+  const composer = useMemo(() => {
+    // Initialize composer
+    const effectComposer = new EffectComposerImpl(gl)
+    // Add render pass
+    effectComposer.addPass(new RenderPass(scene, camera))
+
+    // Create & add a halftone pass
+    const halftoneParams = {
+      shape: 1,
+      radius: 2,
+      rotateR: Math.PI / 12,
+      rotateB: (Math.PI / 12) * 2,
+      rotateG: (Math.PI / 12) * 3,
+      scatter: 1,
+      blending: 1,
+      blendingMode: 1,
+      greyscale: false,
+      disable: false,
+    }
+    const halftonePass = new HalftonePass(
+      size.width,
+      size.height,
+      halftoneParams
+    )
+    effectComposer.addPass(halftonePass)
+
+    return effectComposer
+  }, [gl, scene, camera, size])
 
   useEffect(() => composer?.setSize(size.width, size.height), [composer, size])
   useFrame(
